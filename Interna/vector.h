78f5436eaf31907fe2114d747ec423ac56ef9ae5
Richard <rrruales@espol.edu.ec>
2017-11-14 22:45:34 -0500

    Segunda parte de taller: Creacion de vector con typedef struct

--- Interna/vector.h
+++ Interna/vector.h
@@ -1,3 +1,8 @@
 void multvec(int *x, int *y, int *z, int n);
 
 void addvec(int *x, int *y, int *z, int n);
+
+typedef struct{
+	int uno;
+	int dos;
+}vector[1];
--- main2.c
+++ main2.c
@@ -3,16 +3,29 @@
 #include <stdio.h>
 #include "vector.h"
 
-int x[2] = {1, 2};
+/*int x[2] = {1, 2};
 int y[2] = {3, 4};
-int z[2];
+int z[2];*/
 
 int main() 
 {
-    addvec(x, y, z, 2);
-    printf("z = [%d %d]\n", z[0], z[1]);
-    multvec(x, y, z, 2);
-    printf("z = [%d %d]\n", z[0], z[1]);
+    printf("Ingresar valor para vector 1.1: \n");
+    vector v1;
+    scanf("%d",&v1->uno);
+    printf("Ingresar valor para vector 1.2: \n");
+    scanf("%d",&v1->dos);
+    printf("Ingresar valor para vector 2.1: \n");
+    vector v2;
+    scanf("%d",&v2->uno);
+    printf("Ingresar valor para vector 2.2: \n");
+    scanf("%d",&v2->dos);
+    vector v3;
+    addvec(v1, v2, v3, 2);
+    printf("El resultado de addvec es:\n");
+    printf("z = [%d %d]\n", v3->uno, v3->dos);
+    multvec(v1, v2, v3, 2);
+    printf("El resultado de multvec es:\n");
+    printf("z = [%d %d]\n", v3->uno, v3->dos);
     return 0;
 }
 /* $end main2 */
